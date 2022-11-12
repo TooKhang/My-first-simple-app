316f8122f706bc246aeee128891ac33a552c7b29
TooKhang <82698798+TooKhang@users.noreply.github.com>
2022-11-12 21:55:37 +0800

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,9 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  TextEditingController textEditingController = TextEditingController();
-  String name = "";
-
+  TextEditingController num1EditingController = TextEditingController();
+  TextEditingController num2EditingController = TextEditingController();
+  double result = 0.0;
 
   @override
   Widget build(BuildContext context) {
@@ -23,26 +23,81 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Simple App'),
         ),
         body: Center(
-          child: Column(
-            children: [
-              const Text("Enter your text below"),
-              TextField(
-                controller: textEditingController,
-              ),
-              ElevatedButton(onPressed: onPressed, child: const Text("Press Me!")),
-              Text(name)
-            ],
-          ),
+          child: Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    const Text(
+                      "Simple Calculator",
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                    TextField(
+                        controller: num1EditingController,
+                        decoration: InputDecoration(
+                            hintText: 'First number',
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0))),
+                        keyboardType: const TextInputType.numberWithOptions()),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    TextField(
+                      controller: num2EditingController,
+                      decoration: InputDecoration(
+                          hintText: 'Second number',
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0))),
+                      keyboardType: const TextInputType.numberWithOptions(),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        ElevatedButton(
+                            onPressed: () => {onPressed("+")},
+                            child: const Text("+")),
+                        ElevatedButton(
+                            onPressed: () => {onPressed("-")},
+                            child: const Text("-")),
+                        ElevatedButton(
+                            onPressed: () => {onPressed("x")},
+                            child: const Text("x")),
+                        ElevatedButton(
+                            onPressed: () => {onPressed("/")},
+                            child: const Text("/")),
+                      ],
+                    ),
+                    Text("Result " + result.toString())
+                  ],
+                ),
+              )),
         ),
       ),
     );
   }
 
-  void onPressed() {
+  void onPressed(String op) {
     setState(() {
-      name = textEditingController.text;
+      double num1 = double.parse(num1EditingController.text);
+      double num2 = double.parse(num2EditingController.text);
+      switch (op) {
+        case "+":
+          result = num1 + num2;
+          return;
+        case "-":
+          result = num1 - num2;
+          return;
+        case "x":
+          result = num1 * num2;
+          return;
+        case "/":
+          result = num1 / num2;
+          return;
+      }
     });
-    print(name);
-
   }
 }
